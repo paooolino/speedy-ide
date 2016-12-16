ebd426f1e9d2559d40d53f2aca6a17298dd74791
Paolo Savoldi <paooolino@gmail.com>
2016-12-16 17:09:57 +0100

    App interaction with tree

--- src/components/App.js
+++ src/components/App.js
@@ -3,52 +3,81 @@ import React, { Component } from 'react';
 import Layout from './Layout';
 import FileTree from './FileTree';
 
-const nodes = [
-	{
-		id: 1,
-		name: "root",
-		children: [{
-			id: 2,
-			"name": "file1"
-		},{
-			id: 3,
-			"name": "file2"
-		},{
-			id: 4,
-			"name": "dir",
-			children: [{
-				id: 5,
-				name: "file3"
-			}, {
-				id: 6,
-				name: "file4"
-			}]
-		}]
-	}, {
-		id: 7,
-		name: "root2",
-		children: [{
-			id: 8,
-			name: "children2"
-		}]
-	}
-];
-
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			openedFolders: [],
+			selectedLeaves: [],
+			nodes: []
+		};
+	}
+	
+	componentWillMount() {
+		this.setState({
+			openedFolders: [],
+			selectedLeaves: [],
+			nodes: [
+				{
+					id: 1,
+					name: "root",
+					children: [{
+						id: 2,
+						"name": "file1"
+					},{
+						id: 3,
+						"name": "file2"
+					},{
+						id: 4,
+						"name": "dir",
+						children: [{
+							id: 5,
+							name: "file3"
+						}, {
+							id: 6,
+							name: "file4"
+						}]
+					}]
+				}, {
+					id: 7,
+					name: "root2",
+					children: [{
+						id: 8,
+						name: "children2"
+					}]
+				}
+			]
+		});
+	}
+	
 	render() {
 		return(
 			<Layout 
 				handlerPos={15}
 				contentHeader={<span>Speedy</span>}
 				contentLeft={<FileTree 
-					nodes={nodes}
-					openedFolders={[1,4,7]}
-					selectedLeaves={[8]}
+					nodes={this.state.nodes}
+					openedFolders={this.state.openedFolders}
+					selectedLeaves={this.state.selectedLeaves}
 					onClickLeaf={(evt, entry)=>{
-						console.log(entry);
+						this.setState({
+							selectedLeaves: [entry.id]
+						});
 					}}		
 					onClickFolder={(evt, entry)=>{
-						console.log(entry);
+						this.setState({
+							selectedLeaves: [entry.id],
+							openedFolders: (() => (
+								this.state.openedFolders.indexOf(entry.id) == -1 ? 
+									// The folder is not opened yet: add to opened
+									[...this.state.openedFolders, entry.id] : 
+									// The folder is opened yet:
+									this.state.openedFolders.filter((v)=>{
+										// remove from opened only if is selected.
+										return v == entry.id && this.state.selectedLeaves.indexOf(entry.id) > -1 ? false : true;
+									})
+							))()
+						});
 					}}					
 				/>}
 			/>
